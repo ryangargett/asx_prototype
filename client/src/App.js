e53dd5d4186f97c3e6e26216372011e5289c5788
ryangargett <r.d.gargett@gmail.com>
2024-12-23 20:06:45 +0800

    basic stylings / layout

--- client/src/App.js
+++ client/src/App.js
@@ -4,12 +4,42 @@ function App() {
   return (
     <main>
       <header>
-        <a href="" classname="logo">ASX Blog</a>
+        <a href="" className="logo">ASX Blog</a>
         <nav>
           <a href="">Login</a>
           <a href="">Register</a>
         </nav>
       </header>
+      <div className="post">
+        <div className="post-img">
+          <img src="https://techcrunch.com/wp-content/uploads/2022/12/lawnmower-Large.jpeg?resize=1200,735"/> 
+        </div>
+        <div className="post-info">
+          <h2>EcoFlow teases full-house battery backup coming later this year</h2>
+          <time>December 15, 2022</time>
+          <p>Today at its special launch event, home backup power giant EcoFlow launched a flurry of new products, including a “Whole-Home Backup Power Solution.”</p>
+        </div>
+      </div>
+      <div className="post">
+        <div className="post-img">
+          <img src="https://techcrunch.com/wp-content/uploads/2022/12/lawnmower-Large.jpeg?resize=1200,735"/> 
+        </div>
+        <div className="post-info">
+          <h2>EcoFlow teases full-house battery backup coming later this year</h2>
+          <time>December 15, 2022</time>
+          <p>Today at its special launch event, home backup power giant EcoFlow launched a flurry of new products, including a “Whole-Home Backup Power Solution.”</p>
+        </div>
+      </div>
+      <div className="post">
+        <div className="post-img">
+          <img src="https://techcrunch.com/wp-content/uploads/2022/12/lawnmower-Large.jpeg?resize=1200,735"/> 
+        </div>
+        <div className="post-info">
+          <h2>EcoFlow teases full-house battery backup coming later this year</h2>
+          <time>December 15, 2022</time>
+          <p>Today at its special launch event, home backup power giant EcoFlow launched a flurry of new products, including a “Whole-Home Backup Power Solution.”</p>
+        </div>
+      </div>
     </main>
   );
 }
