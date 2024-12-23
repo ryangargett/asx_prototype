d741364fca894a8d87fde45a3d32079af0f68ffc
ryangargett <r.d.gargett@gmail.com>
2024-12-23 19:18:09 +0800

    project init

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+  return (
+    <main>
+      <header>
+        <a href="" classname="logo">ASX Blog</a>
+        <nav>
+          <a href="">Login</a>
+          <a href="">Register</a>
+        </nav>
+      </header>
+    </main>
+  );
+}
+
+export default App;
