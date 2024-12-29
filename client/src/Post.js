212c70cdec18c5027ac1638a95df75b21fa3b6ae
ryangargett <r.d.gargett@gmail.com>
2024-12-29 20:13:59 +0800

    improved card css styling

--- client/src/Post.js
+++ client/src/Post.js
@@ -1,11 +1,10 @@
 import TimeAgo from 'javascript-time-ago';
-import en from 'javascript-time-ago/locale/en';
 import enAU from 'javascript-time-ago/locale/en-AU';
 import ReactTimeAgo from 'react-time-ago';
 
 TimeAgo.addLocale(enAU)
 
-export default function Post({post_id, title, cover_image, modified_at}) {
+export default function Post({post_id, title, cover_image, summary, modified_at}) {
     const coverPath = `http://localhost:8000/uploads/${post_id}/${cover_image}`;
     console.log(modified_at);
     const modifiedAt = new Date(modified_at);
@@ -16,12 +15,13 @@ export default function Post({post_id, title, cover_image, modified_at}) {
         <div className="post" id={post_id}>
             <div className="post-img">
                 <img 
-                    src={coverPath}  
+                    src={coverPath}
+                    alt={title}
                 /> 
             </div>
             <div className="post-info">
-                <h2>{title}</h2>
-                <h3>
+                <h1 className="post-title">{title}</h1>
+                <h3 className="post-date">
                     Posted: <ReactTimeAgo date={localModifiedAt} locale="en-AU"/>
                 </h3>
             </div>
