5741a1b88acc6737fcf8124ea632b46e5ee3681b
ryangargett <r.d.gargett@gmail.com>
2025-05-20 07:42:35 +0800

    improved email template to handle no-article case

--- server/app.py
+++ server/app.py
@@ -1051,5 +1051,4 @@ async def read_root():
     return {"message": "Welcome to the FastAPI application"}
 
 if __name__ == "__main__":
-    #uvicorn.run(app, host="0.0.0.0", port=8000)
-    collect_for_email()
+    uvicorn.run(app, host="0.0.0.0", port=8000)
